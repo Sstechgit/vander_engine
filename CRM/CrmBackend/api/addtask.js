305a6e1152acb244d92e4e39194b6575311f04ba
anek chauhan <anekchauhan26@gmail.com>
2025-03-03 10:48:30 +0530

    dss

--- CRM/CrmBackend/api/addtask.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { AddTask } = require("../controller/taskcontroller");
-
-router.post("/addTask", AddTask);
-
-module.exports = router;
