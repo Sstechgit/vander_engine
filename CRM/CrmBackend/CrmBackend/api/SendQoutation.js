2720cb48640fe4040a995d1ce98968bc3edacd26
Anek Chauhan <anekchauhan01@gmail.com>
2025-03-03 16:20:53 +0530

    new

--- CRM/CrmBackend/CrmBackend/api/SendQoutation.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const Qtrouter = express.Router();
-const  { SendQuotations, ViewQuotations } = require("../controller/SendQuotation");
-
-Qtrouter.post("/SendQuotations", SendQuotations);
-Qtrouter.get("/ViewQuotations", ViewQuotations);
-
-
-module.exports = Qtrouter;
